45b4845e4e06011abe8d2abeb0367b69a33e44ad
Ben van Werkhoven <b.vanwerkhoven@esciencecenter.nl>
2016-07-15 14:14:56 +0200

    added kernel for removing rows and columns from sparse matrix

---
+++ km3net/kernels/remove_nodes.cu
@@ -0,0 +1,54 @@
+
+
+
+#ifndef block_size_x
+#define block_size_x 256
+#endif
+
+
+
+/*
+ * This kernel removes nodes with degree less than or equal to minimum.
+ * For the remaining nodes this kernel removes edges to nodes that have been removed.
+ *
+ * To remove a node we need to set its degree to zero
+ * To remove an edge we need to set its col_idx to -1
+ */
+__global__ void remove_nodes(int *degrees, int *row_idx, int *col_idx, int *prefix_sum, const int *__restrict__ minimum, int n) {
+    int i = blockIdx.x * block_size_x + threadIdx.x;
+
+    if (i<n) {
+        int my_degree = degrees[i];
+        int min = minimum[0];
+
+        //if my node needs to be removed, remove it
+        if (my_degree > 0 && my_degree <= min) {
+            degrees[i] = 0;
+        }
+        
+        //if my node remains, update my edges
+        if (my_degree > min) {
+
+            //obtain indices to iterate over my edges
+            int start = 0;
+            if (i>0) {
+                start = prefix_sum[i-1];
+            }
+            int end = prefix_sum[i];
+
+            //remove edges to nodes with degree less than or equal to min
+            for (int k=start; k<end; k++) {
+                int col = col_idx[k];
+                if (col != -1 && degrees[col] <= min) {
+                    col_idx[k] = -1;
+                }
+
+            }
+
+        }
+
+
+    }
+}
+
+
---
+++ test/test_remove_nodes_kernel.py
@@ -0,0 +1,96 @@
+from __future__ import print_function
+
+from scipy.sparse import csr_matrix
+import numpy as np
+import os
+from nose.tools import nottest
+
+from .context import skip_if_no_cuda_device, get_kernel_path, create_plot
+
+from kernel_tuner import run_kernel
+
+def test_remove_nodes_kernel():
+
+    skip_if_no_cuda_device()
+
+    #obtain a true correlation matrix from the correlations table
+    def get_full_matrix(correlations):
+        n = correlations.shape[1]
+        matrix = np.zeros((n,n), dtype=np.uint8)
+        for i in range(n):
+            for j in range(correlations.shape[0]):
+                if correlations[j,i] == 1:
+                    col = i+j+1
+                    if col < n and col >= 0:
+                        matrix[i,col] = 1
+                        matrix[col,i] = 1
+        return matrix
+
+    with open(get_kernel_path()+'remove_nodes.cu', 'r') as f:
+        kernel_string = f.read()
+
+    N = np.int32(300)
+    sliding_window_width = np.int32(150)
+    problem_size = (N, 1)
+    params = { "block_size_x": 128 }
+
+    #generate input data with an expected density of correlated hits
+    correlations = np.random.randn(sliding_window_width, N)
+    correlations[correlations <= 2.2] = 0
+    correlations[correlations > 2.2] = 1
+    correlations = np.array(correlations.reshape(sliding_window_width, N), dtype=np.uint8)
+
+    #zero the triangle at the end of correlations table that can not contain any ones
+    for j in range(correlations.shape[0]):
+        for i in range(N-j-1, N):
+            correlations[j,i] = 0
+
+    #obtain full correlation matrix for reference
+    dense_matrix = get_full_matrix(correlations)
+    sparse_matrix = csr_matrix(dense_matrix)
+
+    #setup all kernel inputs
+    degrees = dense_matrix.sum(axis=0).astype(np.int32)
+    prefix_sums = np.cumsum(degrees).astype(np.int32)
+    row_idx = (sparse_matrix.nonzero()[0]).astype(np.int32)
+    col_idx = (sparse_matrix.nonzero()[1]).astype(np.int32)
+    minimum = np.zeros(2).astype(np.int32)
+    minimum[0] = 5 #remove nodes with degree 5 or less, as a test
+
+    #call the CUDA kernel
+    args = [degrees, row_idx, col_idx, prefix_sums, minimum, N]
+    answer = run_kernel("remove_nodes", kernel_string, problem_size, args, params)
+
+    #verify all kernel outputs
+    #updated degrees array
+    print("degrees")
+    print(answer[0])
+    reference = degrees.copy()
+    reference[degrees <= minimum[0]] = 0
+    assert all(answer[0] == reference)
+
+    #updated col_idx array
+    #this array will contain -1 for removed edges of non-removed nodes
+    print("col_idx")
+    print(answer[2])
+    col_idx = answer[2]
+
+    #construct sparse matrix without the removed edges
+    row_idx = row_idx[col_idx!=-1]
+    col_idx = col_idx[col_idx!=-1]
+    answer_matrix = csr_matrix((np.ones_like(row_idx),(row_idx,col_idx)), shape=(N,N))
+
+    #reconstruct the same matrix from the reference answer
+    for i in range(dense_matrix.shape[0]):
+        for j in range(dense_matrix.shape[1]):
+            #for non-removed nodes remove the edge to removed nodes
+            if reference[i] > 0 and reference[j] == 0:
+                dense_matrix[i,j] = 0
+    sparse_ref = csr_matrix(dense_matrix)
+    print(sparse_ref.nnz)
+
+    #verify that the two sparse matrices are the same
+    assert answer_matrix.nnz == sparse_ref.nnz
+    assert all(sparse_ref.nonzero()[0] == answer_matrix.nonzero()[0])
+    assert all(sparse_ref.nonzero()[1] == answer_matrix.nonzero()[1])
+

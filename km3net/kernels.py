bc845d851c9807d0fdffb84d86b967cdcc11ad19
Konrad Karaś <konrad.karas@enxoo.com>
2019-03-21 10:17:12 +0100

    Added LouvainSparse implementation

--- km3net/kernels.py
+++ km3net/kernels.py
@@ -1,4 +1,4 @@
-from __future__ import print_function
+# from __future__ import print_function
 
 import numpy as np
 import pycuda.driver as drv
@@ -347,3 +347,262 @@ class PurgingSparse(object):
         return []
 
 
+class LouvainSparse(object):
+    """ class that provides an interface to the GPU Kernels used for Louvain Community Detection algorithm """
+
+    def __init__(self, N, cc):
+        """instantiate LouvainSparse
+
+        :param N: The largest number of hits that are to be processed by one iteration
+                of the quadratic difference algorithm.
+        :type N: int
+
+        :param cc: The CUDA compute capability of the target device as a string, consisting
+                of the major and minor number concatenated without any separators.
+        :type cc: string
+
+        """
+
+        self.N = N
+
+        with open(get_kernel_path()+'louvain.cu', 'r') as f:
+            louvain_kernel = f.read()
+
+        self.move_nodes = SourceModule(louvain_kernel, options=['-Xcompiler=-Wall'],
+                                       arch='compute_' + cc, code='sm_' + cc,
+                                       cache_dir=False).get_function("move_nodes")
+
+        self.calc_community_degrees = SourceModule(louvain_kernel, options=['-Xcompiler=-Wall'],
+                                                   arch='compute_' + cc, code='sm_' + cc,
+                                                   cache_dir=False).get_function("calculate_community_degrees")
+
+        self.calc_community_inernal = SourceModule(louvain_kernel, options=['-Xcompiler=-Wall'],
+                                                arch='compute_' + cc, code='sm_' + cc,
+                                                   cache_dir=False).get_function("calculate_community_internal_edges")
+
+        self.calc_community_inernal_sum = SourceModule(louvain_kernel, options=['-Xcompiler=-Wall'],
+                                                   arch='compute_' + cc, code='sm_' + cc,
+                                                   cache_dir=False).get_function("calculate_community_internal_sum")
+                                                
+        self.calc_part_modularity = SourceModule(louvain_kernel, options=['-Xcompiler=-Wall'],
+                                                arch='compute_' + cc, code='sm_' + cc,
+                                                cache_dir=False).get_function("calc_part_modularity")
+
+        block_size_x = 128
+        self.max_blocks = (np.ceil(N / float(block_size_x))).astype(np.int32)
+        self.threads = (block_size_x, 1, 1)
+        self.grid = (int(self.max_blocks), 1)
+
+    def compute(self, col_idx, prefix_sums, degrees):
+
+        # check input
+        d_col_idx = ready_input(col_idx)
+        d_prefix_sums = ready_input(prefix_sums)
+        d_degrees = ready_input(degrees)
+
+        h_degrees = np.zeros(self.N).astype(np.int32)
+        drv.memcpy_dtoh(h_degrees, d_degrees)
+        h_total_m = np.int32(h_degrees.sum()/2)
+        d_total_m = allocate_and_copy(h_total_m)
+
+        # copy col_idx to local
+        h_col_idx = np.zeros(h_degrees.sum()).astype(np.int32)
+        drv.memcpy_dtoh(h_col_idx, d_col_idx)
+
+        # copy prefix_sums to local
+        h_prefix_sums = np.zeros(self.N).astype(np.int32)
+        drv.memcpy_dtoh(h_prefix_sums, d_prefix_sums)
+
+        #init state: each vertex represents its own community
+        h_community_degrees = h_degrees
+        d_community_degrees = allocate_and_copy(h_community_degrees)
+        # initially community index represents node index
+        h_community_idx = np.arange(self.N).astype(np.int32)
+        d_community_idx = allocate_and_copy(h_community_idx)
+        
+
+        #check data
+        print('##### INIT GRAPH #####')
+        print('h_community_idx: ' + str(h_community_idx.size))
+        print(h_community_idx)
+        print('h_prefix_sums: ' + str(h_prefix_sums.size))
+        print(h_prefix_sums)
+        print('h_degrees: ' + str(h_degrees.size))
+        print(h_degrees)
+        print('h_community_degrees: ' + str(h_community_degrees.size))
+        print(h_community_degrees)
+        
+
+        # argument list for calculating node moves
+        d_tmp_community_idx = allocate_and_copy(np.zeros(self.N).astype(np.int32))
+        args_move_nodes = [d_col_idx, d_prefix_sums, d_degrees, d_community_idx, d_community_degrees, d_tmp_community_idx, d_total_m]
+
+        # argument list for calculating node moves
+        d_tmp_community_degrees = allocate_and_copy(np.zeros(self.N).astype(np.int32))
+        args_calc_comm_deg = [d_tmp_community_idx, d_degrees, d_tmp_community_degrees]
+        
+        # argument list for calculating inter-connecting edges within communities
+        d_tmp_community_inter = allocate_and_copy(np.zeros(self.N).astype(np.int32))
+        args_calc_comm_inter = [d_col_idx, d_prefix_sums, d_tmp_community_idx, d_tmp_community_inter]
+
+        # argument list for calculating inter-connecting edges within communities
+        d_tmp_community_inter_sum = allocate_and_copy(np.zeros(self.N).astype(np.int32))
+        args_calc_comm_inter_sum = [d_tmp_community_idx, d_tmp_community_inter, d_tmp_community_inter_sum]
+
+        # argument list for calculating modulatities
+        d_part_mod = allocate_and_copy(np.zeros(self.N).astype(np.float32))
+        args_part_mod = [d_tmp_community_inter_sum, d_tmp_community_degrees, d_total_m, d_part_mod]
+
+
+        total_iterations = 0
+        while True:
+            # for debug
+            modularities = []
+
+            #get data from device
+            drv.memcpy_dtoh(h_degrees, d_degrees)
+
+            # START LOUVAIN
+
+            phase_modularity = 0
+
+            while True:
+                # 1: Calculate best node moves
+                self.move_nodes(*args_move_nodes, block=self.threads, grid=self.grid)
+
+                # tmp_community_idx = np.zeros(self.N).astype(np.int32)
+                # drv.memcpy_dtoh(tmp_community_idx, d_tmp_community_idx)
+                # print('-----------------------------------------------')
+                # print('ITERATION RESULT:')
+                # print(tmp_community_idx)
+
+                # 2: Calculate degrees of communities
+                self.calc_community_degrees(*args_calc_comm_deg, block=self.threads, grid=self.grid)
+
+                # tmp_community_degrees = np.zeros(self.N).astype(np.int32)
+                # drv.memcpy_dtoh(tmp_community_degrees, d_tmp_community_degrees)
+                # print('NEW tmp_community_degrees:')
+                # print(tmp_community_degrees)
+
+                # 3: Calculate inter-connecting edges in communities
+                # 3.1: Calculate inter-connecting edges per node within same community
+                self.calc_community_inernal(*args_calc_comm_inter, block=self.threads, grid=self.grid)
+                self.calc_community_inernal_sum(*args_calc_comm_inter_sum, block=self.threads, grid=self.grid)
+
+                # tmp_internal = np.zeros(self.N).astype(np.int32)
+                # drv.memcpy_dtoh(tmp_internal, d_tmp_community_inter_sum)
+                # print('internal_links:')
+                # print(tmp_internal)
+
+                # 4: Calculate graph modularity
+                # 4.1: Calculate partial modularity per community
+                self.calc_part_modularity(*args_part_mod, block=self.threads, grid=self.grid)
+                part_mod = np.zeros(self.N).astype(np.float32)
+                drv.memcpy_dtoh(part_mod, d_part_mod)
+
+                # print('part_mods:')
+                # print(part_mod)
+
+                # 4.2: Summarize modularities (additive function)
+                current_mod = 0
+                for i in range(self.N):
+                    current_mod += part_mod[i]
+
+                modularities = np.concatenate((modularities, [current_mod]))
+
+                if (abs(current_mod) <= abs(phase_modularity)):
+                    break
+
+                phase_modularity = current_mod
+
+                # replace buffers
+                drv.memcpy_dtoh(h_community_idx, d_tmp_community_idx)
+                drv.memcpy_dtoh(h_community_degrees, d_tmp_community_degrees)
+                drv.memcpy_htod(d_community_idx, h_community_idx)
+                drv.memcpy_htod(d_community_degrees, h_community_degrees)
+
+                # THIS REASSIGNMENT DOESNT WORK:
+                # d_community_idx = d_tmp_community_idx
+                # d_community_degrees = d_tmp_community_degrees
+
+                
+            print('######## MODULARITY GAIN #########')
+            print(modularities)
+            print('######## PHASE MODULARITY #########')
+            print(phase_modularity)
+
+            print('######## CURRENT ASSIGNMENTS ITERATION [' + str(total_iterations) + '] #########')
+            print(h_community_idx)
+
+            # merge communities
+
+            # Replace col_idx with community_idx
+            comm_col_idx = np.zeros(h_community_degrees.sum()).astype(np.int32)
+            new_col_last = 0
+
+            for i in range(self.N):
+                for n in range(self.N):
+                    if (h_community_idx[n] == i):
+                        start = 0
+                        if (n > 0):
+                            start = h_prefix_sums[n - 1]
+                        end = h_prefix_sums[n]
+
+                        for j in range(start, end):
+                            col = h_col_idx[j]
+                            comm_col_idx[new_col_last] = h_community_idx[col]
+                            new_col_last += 1
+
+            comm_prefix_sum = np.cumsum(h_community_degrees)
+
+            new_col_idx = []
+            # new_weights = []
+            new_degrees_weighted = np.zeros(self.N).astype(np.int32)
+            new_community_degrees = np.zeros(self.N)
+            for i in range(self.N):
+                start = 0
+                if (i > 0):
+                    start = comm_prefix_sum[i - 1]
+                end = comm_prefix_sum[i]
+
+                corr_hits = comm_col_idx[start:end]
+                unique_hits = np.unique(corr_hits, return_counts=True)
+                new_col_idx = np.concatenate((new_col_idx, unique_hits[0]))
+                new_community_degrees[i] = len(unique_hits[0])
+                new_degrees_weighted[i] = unique_hits[1].sum()
+
+            new_col_idx = np.array(new_col_idx).astype(np.int32)
+            new_prefix_sums = np.array(np.cumsum(new_community_degrees)).astype(np.int32)
+
+            print('########## NEW GRAPH ##############')
+            print('new_col_idx ' + str(new_col_idx.size))
+            print(new_col_idx)
+            print('new_prefix_sums ' + str(new_prefix_sums.size))
+            print(new_prefix_sums)
+            print('new_degrees_weighted ' + str(new_degrees_weighted.size))
+            print(new_degrees_weighted)
+            # print('new_weights ' + str(new_weights.size))
+            # print(new_weights)
+            # print('new_community_degrees ' + str(new_community_degrees.size))
+            # print(new_community_degrees)
+            # print('new_total_hits: ' + str(new_community_degrees.sum()))
+
+            # CREATE NEW GRAPH
+            h_col_idx = new_col_idx
+            h_prefix_sums = new_prefix_sums
+            h_degrees = new_degrees_weighted
+            h_community_degrees = new_degrees_weighted
+
+            drv.memcpy_htod(d_col_idx, h_col_idx)
+            drv.memcpy_htod(d_prefix_sums, h_prefix_sums)
+            drv.memcpy_htod(d_degrees, h_degrees)
+
+            drv.memcpy_htod(d_community_degrees, h_community_degrees)
+            
+
+            total_iterations +=1
+            if (total_iterations == 5):
+                break
+                    
+        
+        return h_community_idx
---
+++ km3net/kernels/louvain.cu
@@ -0,0 +1,182 @@
+#ifndef block_size_x
+#define block_size_x 128
+#endif
+
+// #define DEBUG_MODE 1
+
+#ifndef gamma_mod
+#define gamma_mod 1
+#endif
+
+#include <stdio.h>
+#include <cmath>
+
+__global__ void move_nodes(int *col_idx, int *prefix_sum, int *degrees, int *community_idx, int *community_degrees, int *tmp_community_idx, int *m_tot) {
+    const int graph_size = 34;
+    const int m = *m_tot;
+    int ti = blockIdx.x * block_size_x + threadIdx.x;
+
+
+    for(int i = ti; i < graph_size; i += block_size_x ) {
+
+        //define neighbour range
+        int start = 0;
+        if (i>0) {
+            start = prefix_sum[i-1];
+        }
+        int end = prefix_sum[i];
+
+        //modularity
+        int current_comm = community_idx[i];
+        int new_comm = current_comm;
+        float local_q = 0;
+        float max_q = 0;
+
+        //iterate over neighbours of i 
+        for(int j = start; j < end; j++) {
+
+            int col = col_idx[j];
+            
+
+            //get community of neighbour
+            int col_comm = community_idx[col];
+
+            int l_i_comm = 0;   //number of edges joining i with community
+            int k_comm = community_degrees[col_comm];     //degree of community
+
+            // The singlet minimum HEURISTIC
+            if(i == current_comm && degrees[i] == community_degrees[current_comm] && col == col_comm && degrees[col] == k_comm && col_comm > current_comm) {
+                #ifdef DEBUG_MODE
+                if(ti == 0) {
+                    printf("$$$");
+                    printf("SKIP CHANGE %d to %d \n", i, col);
+                    printf("$$$");
+                }
+                #endif
+
+                continue;
+            }
+
+            //search for other neighbors from this community
+            for(int n = start; n < end; n++) {
+                int col_n = col_idx[n];
+                //check if its from the same community
+                if(community_idx[col_n] != col_comm) {
+                    continue;
+                }
+                l_i_comm++;
+            }
+
+            // local_q = (1.0 / (float)graph_size) * ((float)l_i_comm - ((float)degrees[i] * (float)k_comm / (2.0 * (float)graph_size)));
+            local_q = (1.0 / (float)m) * ((float)l_i_comm - ((float)degrees[i] * (float)k_comm / (2.0 * (float)m)));
+            // local_q = (1 / (2* (float)m_tot)) * ( l_i_comm - (k_comm * (float)degrees[i] / (float)m_tot) );
+
+            #ifdef DEBUG_MODE
+            if(ti == 0) {
+                printf("=============== \n");
+                printf("migrate %d to %d \n", i, col_comm);
+                printf("m_tot = %d \n", m);
+                printf("l_i_comm = %d \n", l_i_comm);
+                printf("degrees[i] = %d \n", degrees[i]);
+                printf("k_comm = %d \n", k_comm);
+                printf("local_q = %f \n", local_q);
+            }
+            #endif
+
+            if(local_q >= max_q) {
+                if(local_q == max_q && new_comm < col_comm) {
+                    //do nothing
+                } else {
+                    #ifdef DEBUG_MODE
+                    if(ti ==0) {
+                        printf("$$$$$ \n");
+                        printf("migrated [%d] from %d to %d \n", i, new_comm, col_comm);
+                        printf("previous q: %f , current q: %f \n", max_q, local_q);
+                        printf("$$$$$ \n");
+                    }
+                    #endif
+
+                    new_comm = col_comm;
+                    max_q = local_q;
+                }
+
+                
+                
+            }
+        }
+
+        tmp_community_idx[i] = new_comm;
+    }
+}
+
+__global__ void calculate_community_degrees(int *community_idx, int *degrees, int *community_degrees) {
+    const int graph_size = 34;
+    int ti = blockIdx.x * block_size_x + threadIdx.x;
+
+    for(int i = ti; i < graph_size; i += block_size_x ) {
+        int com_deg = 0;
+        for(int j = 0; j < graph_size; j++) {
+            if(community_idx[j] == i) {
+                com_deg += degrees[j];
+            }
+        }
+        community_degrees[i] = com_deg;
+    }
+}
+
+__global__ void calculate_community_internal_edges(int *col_idx, int *prefix_sum, int *community_idx, int *community_int_edg) {
+    const int graph_size = 34;
+    int ti = blockIdx.x * block_size_x + threadIdx.x;
+
+    for(int i = ti; i < graph_size; i += block_size_x ) {
+        int inter_count = 0;
+       
+        //define neighbour range
+        int start = 0;
+        if (i>0) {
+            start = prefix_sum[i-1];
+        }
+        int end = prefix_sum[i];
+        int current_comm = community_idx[i];
+
+        //iterate over neighbours of i 
+        for (int j = start; j < end; j++) {
+            int col = col_idx[j];
+            if (community_idx[col] == current_comm) {
+                inter_count++;
+            }
+        }
+
+        community_int_edg[i] = inter_count;
+    }
+}
+
+__global__ void calculate_community_internal_sum(int *community_idx, int *community_int_edg, int *community_internal_sum) {
+    const int graph_size = 34;
+    int ti = blockIdx.x * block_size_x + threadIdx.x;
+
+    for(int i = ti; i < graph_size; i += block_size_x ) {
+        int comm_sum = 0;
+        for(int j = 0; j < graph_size; j++) {
+            if(community_idx[j] == i) {
+                comm_sum += community_int_edg[j];
+            }
+        }
+
+        // edges are bidirectional
+        community_internal_sum[i] = comm_sum / 2;
+    }
+}
+
+__global__ void calc_part_modularity(int *inter_comm_deg, int *community_degrees, int *m_tot, float *part_modularities) {
+    const int graph_size = 34;
+    const int m_ = *m_tot;
+    int ti = blockIdx.x * block_size_x + threadIdx.x;
+
+    for(int i = ti; i < graph_size; i += block_size_x) {
+        float lc = (float)inter_comm_deg[i];
+        float kc = (float)community_degrees[i];
+        float m = (float)m_;
+        part_modularities[i] = ( ( lc/m ) - (pow(kc/(2*m), 2.0f)) );
+    }
+}
